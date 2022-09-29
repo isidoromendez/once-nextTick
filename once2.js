62dc956e4078211b235f3359742be5b0f80da69d
isidoromendez <isidoro.mendez@globant.com>
2022-09-29 11:29:02 -0300

    feat: first commit

---
+++ once2.js
@@ -0,0 +1,16 @@
+const { EventEmitter, once } = require('events');
+
+const myEE = new EventEmitter();
+
+async function foo() {
+  await Promise.all([once(myEE, 'bar'), once(myEE, 'foo')]);
+  console.log('foo', 'bar');
+}
+
+process.nextTick(() => {
+  myEE.emit('bar');
+  myEE.emit('foo');
+  console.log('nextTick')
+});
+
+foo().then(() => console.log('done'));
